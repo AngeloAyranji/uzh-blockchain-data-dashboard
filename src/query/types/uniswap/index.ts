b4d396bac983260d1555c19150eb8b32b0f52371
AngeloAyranji <angeloayranji@gmail.com>
2024-06-01 11:27:24 +0300

    add types

--- src/query/types/uniswap/index.ts
+++ src/query/types/uniswap/index.ts
@@ -9,3 +9,29 @@ export class PoolTotalCount {
     factoryAddress: string;
     factoryVersion: string;
 }
+
+export class PoolCreatedByDate {
+    factoryVersion: string;
+    date: Date;
+    count: number;
+}
+
+export class LatestSwaps {
+    id: string;
+    poolAddress: string;
+    transactionHash: string;
+    sender: string;
+    recipient: string;
+    amountIn: string;
+    amountOut: string;
+    reversed: boolean;
+    timestamp: Date;
+    page: number;
+    limit: number;
+}
+
+export class TopActivePools {
+    poolAddress: string;
+    token0: string;
+    token1: string;
+}
